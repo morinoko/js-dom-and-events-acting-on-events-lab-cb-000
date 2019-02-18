6387ce1999cddc0e9c4c2a5774e2650d479b7ccd
Felice Forby <feliceforby@gmail.com>
2019-02-18 00:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function retrieveEmployeeInformation() {
 function addNewElementAsLi() {
   const employeeList = document.querySelector('.employee-list');
   const employeeName = retrieveEmployeeInformation();
-  
+
   const li = document.createElement('li');
   li.innerHTML = employeeName;
 
